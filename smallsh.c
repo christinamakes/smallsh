fd9994c0a5f5ee9153797d65f035359ce44daa2f
Christina Moore <sayhitochristina@gmail.com>
2022-11-02 11:32:25 -0500

    fgModes

--- smallsh.c
+++ smallsh.c
@@ -14,12 +14,18 @@
 /* Foreground flag */
 int isForeground = 0;
 
-//catch sigtstp
-void catchSigtstp(int);
+//define maxes
+#define ARGS_MAX_SIZE 	512
+#define COMMAND_MAX_SIZE 	2048
+
 //function to parse commands
 void parseCmd(char**, char*[], char[], char[], size_t*, const size_t*, const pid_t*);
 //function to execute commands
 void executeCmd(char*[], char[], char[], struct sigaction, const size_t*, pid_t*, int*, int);
+//function to turn foreground only on
+void fgModeOn(int);
+//function to turn foreground only off
+void fgModeOff(int);
 //function to move dirs
 void moveDir(char*[], const size_t*);
 //print status function
@@ -30,10 +36,6 @@ void exitSmallsh(char**, pid_t*);
 int main(int argc, char *argv[]) {
 //    get pid of shell
     pid_t shellPid = getpid();
-//    max arg size
-    size_t argsMaxSize = 512;
-//    max command size
-    size_t commandMaxSize = 2049;
 //    chars to compare to for commands
     char bkg = '&';
     char comment = '#';
@@ -43,9 +45,8 @@ int main(int argc, char *argv[]) {
     char* exitSmallsh = "exit";
 //    exit flag
     int exitFlag = 0;
-
 //    args
-    char* args[argsMaxSize];
+    char* args[ARGS_MAX_SIZE];
     size_t argsCount = 0;
 //    commands
     char* cmd = NULL;
@@ -65,4 +66,24 @@ int main(int argc, char *argv[]) {
         piece = strtok(NULL, " ");
     };
     return 0;
+}
+
+void catchSigtstp(int sig) {
+    printf("hello catchSigtstp");
+};
+
+void parseCmd(char** command, char* args[], char inPath[], char outPath[], size_t* argsCount, const size_t* argsMaxSize, const pid_t* shellPid) {
+    printf("hello parseCmd");
+};
+
+void fgmModeOn(int sig) {
+//    register fgmode_off for SIGTSTP
+//    write "Foreground only mode on"
+//    fg_only = true
+}
+
+void fgModeOff(int sig) {
+//    register fgmode_on() for SIGTSTP
+//    write "Foreground only mode off"
+//    fg_only = false
 }
