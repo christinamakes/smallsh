5d72dbbde40578dc61692854f33638487ab3cd8f
Christina Moore <sayhitochristina@gmail.com>
2022-11-09 16:49:52 -0600

    spiffy

--- smallsh.c
+++ smallsh.c
@@ -36,58 +36,58 @@ int forkList[100] = {0};
 int forks = 0;
 int forkStatus;
 
-//function to parse commands
+//  function to parse commands
 int parseInput(char* args);
-//function to execute commands
+//  function to execute commands
 void doCmds(void);
-//helper to check for background command
+//  helper to check for background command
 void isBkgCmd(void);
-//function to turn foreground only on/off
-void fgModeToggle(int);
-//function to move dirs
+//  function to move dirs
 void cdCmd(void);
-//print status function
+//  print status function
 void statusCmd(void);
-//exit smallsh
+//  exit smallsh
 void exitCmd(void);
-//exec/fork/waitpid commands handler
+//  execvp/fork/waitpid commands handler
 void execCmds(void);
-//signal handler for SIGTSTP
+//  signal handler for SIGTSTP
 void handle_SIGTSTP(int);
-
+//  sig structs
 struct sigaction SIGTSTP_action = {0};
 struct sigaction SIGINT_action = {0};
 
-int main(int argc, char *argv[]) {
-//  list of raw args from user
-    char rawArgs[ARGS_MAX_SIZE];
 /*
- * main():
-    block SIGTSTP
-    register fgmode_on() for SIGTSTP
-    loop:
-       unblock SIGTSTP
-       read_next_line()
-       block SIGTSTP
-       process_line()
-       fork_exec()
-       if fg or fg_only:
-           wait()
+ * FUNCTION: main(int, char*)
+ * ---------------------------------------
+ * main function
+ *  - Handles signals for SIGINT and SIGTSTP
+ *  - Handles raw input from user, passes to parseInput() for processing
+ *  - Calls doCmds() with processed input
  *
+ * Args:
+ *  - int argc - number of arguments
+ *  - char *argv[] - raw arguments
+ *
+ * Returns: None
  */
-// taken from module 5, SIGINT block (CTRL-C)
+int main(int argc, char *argv[]) {
+    // list of raw args from user
+    char rawArgs[ARGS_MAX_SIZE];
+
+    // taken from module 5, SIGINT block (CTRL-C)
     SIGINT_action.sa_handler = SIG_IGN;
     sigfillset(&SIGINT_action.sa_mask);
     SIGINT_action.sa_flags = 0;
     sigaction(SIGINT, &SIGINT_action, NULL);
 
-//    SIGTSTP (CTRL-Z) handler
+    // SIGTSTP (CTRL-Z) handler
     SIGTSTP_action.sa_handler = handle_SIGTSTP;
-//    from module 5, SA_RESTART - do not block
+    // from module 5, SA_RESTART - do not block
     SIGTSTP_action.sa_flags = SA_RESTART;
     sigfillset(&SIGTSTP_action.sa_mask);
     sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
+    // main driver loop - process input, reset argsGlobal, then pass to doCmds()
     while(1) {
         argsCount = parseInput(rawArgs);
         argsGlobal[argsCount] = NULL;
@@ -106,19 +106,6 @@ int main(int argc, char *argv[]) {
  * Returns: None
  */
 void handle_SIGTSTP(int signo){
-    fgModeToggle(signo);
-}
-
-/*
- * FUNCTION fgModeToggle(int)
- * ---------------------------------------
- * Toggles foreground-only mode on/off when signal is received. Returns message to user.
- *
- * Args: int - foreground signal
- *
- * Returns: None
- */
-void fgModeToggle(int sig) {
     if(isFgMode == 0) {
         isFgMode = 1;
         char *message = "Entering foreground-only mode (& is now ignored)\n";
@@ -137,31 +124,30 @@ void fgModeToggle(int sig) {
 /*
  * FUNCTION: parseInput(char*)
  * ---------------------------------------
- * Takes raw args from user and saves them to the parsedArgs list for later use.
+ * Takes raw args from user and saves them to the argsGlobal for doCmds().
  *
  * Args: args - char* of args[2048]
  *
  * Returns: number of arguments in int form
  */
 int parseInput(char* args) {
-//    prompt user ':' and flush
+    char* argToken;
+    int i;
     int localArgsCount = 0;
+    // prompt user ':' and flush
     printf(": ");
     fflush(stdout);
     fgets(args, COMMAND_MAX_SIZE, stdin);
-//    remove \n from rawArgs
+    // remove \n from rawArgs
     strtok(args, "\n");
 
-//    taken from https://beej.us/guide/bgc/html/split/stringref.html#man-strtok - common strtok usage loop
-    char* argToken;
-    int i;
+    // taken from https://beej.us/guide/bgc/html/split/stringref.html#man-strtok - common strtok usage loop
     if ((argToken = strtok(args, " ")) != NULL) {
         do {
-//            reset background mode
+            // reset background mode
             if (isBkg == 1) {
                 isBkg = 0;
             };
-
             // put argToken in global args list
             argsGlobal[localArgsCount] = argToken;
            // handle $$ expansion
@@ -190,20 +176,19 @@ int parseInput(char* args) {
  */
 void doCmds(void) {
     char* command = argsGlobal[0];
-
-//    command is comment '#'
+    // command is comment '#'
     if (command[0] == '#' || command[0] == '\n') {
-//       eat comments and new lines
+        // eat comments and new lines
     }
-//    command is 'cd'
+    // command is 'cd'
     else if (strcmp(command, moveDir) == 0) {
         cdCmd();
     }
-//    command is 'status
+    // command is 'status
     else if (strcmp(command, status) == 0) {
         statusCmd();
     }
-//    command is 'exit'
+    // command is 'exit'
     else if (strcmp(command, exitSmallsh) == 0) {
         exitCmd();
     }
@@ -215,7 +200,7 @@ void doCmds(void) {
 /*
  * FUNCTION isBkgCmd()
  * ---------------------------------------
- * Helper for doCmds. Checks if entered command is background and sets global flag
+ * Checks if entered command is background and sets global flag
  *
  * Args: None
  *
@@ -224,7 +209,7 @@ void doCmds(void) {
 void isBkgCmd() {
     char* lastCommand = argsGlobal[argsCount - 1];
     if (strcmp(lastCommand, bkg) == 0) {
-//        don't allow bkg if foreground only mode on
+        // don't allow bkg if foreground only mode on
         if (isFgMode == 1) {
             isBkg = 0;
         } else isBkg = 1;
@@ -244,21 +229,19 @@ void isBkgCmd() {
  */
 void cdCmd() {
     int errorFlag = 0;
-//    no dir specified
+    // no dir specified
     if (argsCount == 1) {
-//        E.T. go home
+        // E.T. go home
         errorFlag = chdir(getenv("HOME"));
     }
     else {
         errorFlag = chdir(argsGlobal[1]);
     }
-
     if (errorFlag != 0) {
         printf("Error cd failed\n");
         fflush(stdout);
     }
 }
-
 /*
  * FUNCTION statusCmd(int)
  * ---------------------------------------
@@ -294,7 +277,6 @@ void statusCmd() {
  */
 void exitCmd() {
     int i;
-    if (forks != 0) printf("Terminating processes...\n");
     for(i = 0; i < forks; i++) kill(forkList[i], SIGTERM);
     printf("Exiting smallsh, goodbye.\n");
     exit(0);
@@ -307,27 +289,26 @@ void exitCmd() {
  *
  * Source: Module 4
  *
- * Args: int - 1 or 0 statusCalled flag
+ * Args: None
  *
  * Returns: None
  */
 void execCmds() {
 // Taken from Module 4
+    int i;
+    char* inputFile = NULL;
+    char* outputFile = NULL;
+    char* lastCommand = argsGlobal[argsCount - 1];
+
     pid_t spawnPid = fork();
     // add to list of forks for exit call
     forkList[forks] = spawnPid;
     forks++;
     // to trim &
-    char* lastCommand = argsGlobal[argsCount - 1];
-
     if (strcmp(lastCommand,"&") == 0) {
-        // remove &
         isBkgCmd();
         argsGlobal[argsCount - 1] = NULL;
 }
-    int i;
-    char* inputFile = NULL;
-    char* outputFile = NULL;
     switch(spawnPid) {
         // error
         case -1:
@@ -396,7 +377,7 @@ void execCmds() {
     }
         // keep checking to see if bkg forks are done, then get their status
         while ((spawnPid = waitpid(-1, &forkStatus, WNOHANG)) > 0) {
-                printf("Background pid %d is done: ", spawnPid);
+                printf("Background pid %d is done. ", spawnPid);
                 fflush(stdout);
                 statusCmd();
             }
